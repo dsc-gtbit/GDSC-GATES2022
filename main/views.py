bf9661122c8c6277c4bd59e5bdec7eea93515aa9
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-04-25 16:28:24 +0530

    Changes in Developer Page

--- main/views.py
+++ main/views.py
@@ -18,7 +18,92 @@ def about(request):
 
 def developer(request):
     context = {
-        "title": "Raghav Dhingra | Developer | GATES 2020",
+        "title": "Developers | GATES 2022",
+        "developers": [{
+            "name": "Aanchal Rakheja",
+            "title": "Full Stack developer",
+            "desc": "Invest your time in technology and you will never regret",
+            "email": "aanchalrakheja2001@gmail.com",
+            "facebook" : "",
+            "github" : "https://github.com/aanchalrakheja",
+            "twitter" : "",
+            "instagram" : "",
+            "linkedIn":"www.linkedin.com/in/aanchal-rakheja2901",
+            "image": "assets/developer/aanchal.png",
+            "website": ""                 
+        },{
+            "name": "Garvit Vij",
+            "title": "Full Stack Developer",
+            "desc": "Effectuating ideas with innovation",
+            "email": "garvit.vij.26@gmail.com",
+            "facebook" : "https://www.facebook.com/garvit.vij.1",
+            "github" : "https://github.com/garvitvij",
+            "twitter" : "https://twitter.com/garvitvij",
+            "instagram" : "https://www.instagram.com/garvit.vij_/",
+            "linkedIn":"https://www.linkedin.com/in/Garvitvij",
+            "image": "assets/developer/garvit.png",
+            "website": ""                        
+        },{
+            "name": "Kanishk Chhabra",
+            "title": "Android & Full Stack Web Developer",
+            "desc": "I Love to Innovate and change lives using Science & Technology",
+            "email": "kanishkchhabra23@gmail.com",
+            "facebook" : "https://facebook.com/mrkc2303",
+            "github" : "https://github.com/mrkc2303",
+            "twitter" : "https://twitter.com/mrkc2303/",
+            "instagram" : "https://www.instagram.com/kanishk_chhabra/",
+            "linkedIn":"https://www.linkedin.com/in/kanishk-chhabra/",
+            "image": "assets/developer/kanishk.png",
+            "website": "https://www.kanishkchhabra.in/"                         
+        },{
+            "name": "Raghav Dhingra",
+            "title": "Full Stack Developer",
+            "desc": "Always try to solve a problem in a smarter way.",
+            "email": "raghavdhingra.com",
+            "facebook" : "",
+            "github" : "",
+            "twitter" : "",
+            "instagram" : "",
+            "linkedIn":"https://www.linkedin.com/in/raghav-dhingra/",
+            "image": "assets/developer/raghav.png",
+            "website": ""                        
+        },{
+            "name": "Ruhee Jain",
+            "title": "Full Stack Web Developer",
+            "desc": "Innovate. Create. Design. Inspire",
+            "email": "ruhj707@gmail.com",
+            "facebook" : "https://www.facebook.com/ruhee.jain16/",
+            "github" : "https://github.com/Ruheej1",
+            "twitter" : "https://twitter.com/Ruheejain_",
+            "instagram" : "https://www.instagram.com/ruheejain_/",
+            "linkedIn":"https://www.linkedin.com/in/ruhee-jain-47447b196/",
+            "image": "assets/developer/ruhee.png",
+            "website": ""                    
+        },{
+            "name": "Rupin Vijan",
+            "title": "Full stack developer",
+            "desc": "I am a web developer with impeccable working skills. Being a diligent learner, I have learnt Web Development  and I work hard to achieve the desired goal.",
+            "email": "rupinvijan@gmail.com",
+            "facebook" : "https://www.facebook.com/profile.php?id=100011423601329",
+            "github" : "https://github.com/RupinVijan",
+            "twitter" : "",
+            "instagram" : "https://www.instagram.com/rupin_vijan/",
+            "linkedIn":"https://www.linkedin.com/in/rupin-v-980b63118",
+            "image": "assets/developer/rupin.png",
+            "website": ""   
+        },{
+            "name": "Unnat Das",
+            "title": "Front-end developer",
+            "desc": "A Front-end developer and Competitive programmer, always keep trying to improve and be a better version of myself.",
+            "email": "udas4153@gmail.com",
+            "facebook" : "",
+            "github" : "https://github.com/Sayo1305",
+            "twitter" : "",
+            "instagram" : "",
+            "linkedIn":"https://www.linkedin.com/in/unnat-das-3b5374196/",
+            "image": "assets/developer/unnat.png",
+            "website": ""                          
+        }],
     }
     return render(request,"developer.html",context)
 
